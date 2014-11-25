040db3c5b3a7d16696dd19bb1f54ebd618823018
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-25 09:58:24 -0600

    Added play again feature, bugs fixed

--- high_low_game.php
+++ high_low_game.php
@@ -1,40 +1,64 @@
 <?php
 
-$num = rand(1, 100);
+// exercise 3.2.1.
+
+$num = mt_rand($argv[1], $argv[2]);
 
 $quip = ['Nice try.', 'Not quite.', 'Try not to hurt yourself...', 
 'Most amusing.', 'Are you even paying attention?',
- 'Come onnnn... think.', 'Congrats, you win - PSYCH.', 
- 'Maybe you\'re not cut out for this.', 
- 'Wrong, but your dedication is impressive.'];
+'Come onnnn... think.', 'Congrats, you win - PSYCH.', 
+'Maybe you\'re not cut out for this.', 
+'Wrong, but your dedication is impressive.'];
 
 do {
 
-fwrite(STDOUT, 'What number am I thinking of? ') . PHP_EOL;
+$iteration = 0;
 
-$guess = trim(fgets(STDIN));
+	do {
 
-$index = rand(0, 8);
- 	
-	if ($guess > $num) {
+// Challenge from Jill - find a way to automatically adjust the max
+// index key value if keys are added or removed
 
-		echo "{$quip[$index]} LOWER!" . PHP_EOL;
-	} 
+		$index = mt_rand(0, 8);
+
+		if ($iteration == 0) {
+
+			fwrite(STDOUT, "I'm thinking of a number between {$argv[1]} and {$argv[2]}. Try to guess it! ") . PHP_EOL;
+			$iteration++;	
+		}
+
+		$guess = trim(fgets(STDIN));
+
+
+		if ($guess > $num) {
+
+			echo "{$quip[$index]} LOWER!" . PHP_EOL;
+		} 
 		
-	elseif ($guess < $num) {
+		elseif ($guess < $num) {
 
 
-		echo "{$quip[$index]} HIGHER!" . PHP_EOL;
+			echo "{$quip[$index]} HIGHER!" . PHP_EOL;
 
-	} 
+		} 
 
-	elseif ($guess == $num) {
+		elseif ($guess == $num) {
 
-		echo "Good guess, you win!" . PHP_EOL;
+			echo "Good guess, you win!" . PHP_EOL;
 
-	}
-	
+			fwrite(STDOUT, "Play again? ") . PHP_EOL;
 
- } while ($guess != $num);
+			$play = trim(fgets(STDIN));			
 
-?>
+			}
+
+		} while ($guess != $num);
+
+} while ($play == 'yes');
+
+
+
+// Cool ideas - enter #guesses count, and start script over again
+ // if 'yes' entered at end of game
+
+	?>
