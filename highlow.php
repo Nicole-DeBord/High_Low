aa6fe740bf0b7fa38e2693433731b38476382065
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-11 17:27:08 -0600

    Confirmed code functionality, edited for readability

---
+++ highlow.php
@@ -0,0 +1,45 @@
+<?php
+// exercise 3.2.1.
+$num = mt_rand($argv[1], $argv[2]);
+$quip = ['Nice try.', 'Not quite.', 'Try not to hurt yourself...', 
+'Most amusing.', 'Are you even paying attention?',
+'Come onnnn... think.', 'Congrats, you win - PSYCH.', 
+'Maybe you\'re not cut out for this.', 
+'Wrong, but your dedication is impressive.'];
+
+if ($argc != 3) {
+
+	echo "Please enter two numbers." . PHP_EOL;
+	trim(fgets(STDIN))
+
+} 
+	$low = min($argv);
+	$high = max($argv[1], $argv[2]);
+	$num = mt_rand($low, $high);
+
+	do {
+		$iteration = 0;
+		do {
+// Challenge from Jill - find a way to automatically adjust the max
+// index key value if keys are added or removed
+			$index = mt_rand(0, 8);
+			if ($iteration == 0) {
+				fwrite(STDOUT, "I'm thinking of a number between {$argv[1]} and {$argv[2]}. Try to guess it! ") . PHP_EOL;
+				$iteration++;	
+			}
+			$guess = trim(fgets(STDIN));
+			if ($guess > $num) {
+				echo "{$quip[$index]} LOWER!" . PHP_EOL;
+			} 
+
+			elseif ($guess < $num) {
+				echo "{$quip[$index]} HIGHER!" . PHP_EOL;
+			} 
+			elseif ($guess == $num) {
+				echo "Good guess, you win!" . PHP_EOL;
+				fwrite(STDOUT, "Play again? ") . PHP_EOL;
+				$play = trim(fgets(STDIN));			
+			}
+		} while ($guess != $num);
+	} while ($play == 'yes');
+?>
